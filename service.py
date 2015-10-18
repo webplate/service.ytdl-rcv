f639076234c5846ced4810e8e58e92e9bfb93982
Glen <glen@mailoo.org>
2015-10-18 02:36:49 +0200

    multithreading

--- service.py
+++ service.py
@@ -1,110 +1,134 @@
+# youtube_dl needs unicode
+from __future__ import unicode_literals
+# standard lib
 import os
-import threading
+import io
+import sys
+import time
 import datetime
-# server module
+import threading
+import Queue
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 # kodi module
 import xbmc
 import xbmcaddon
-# youtube-dl control
-import YDStreamUtils
-import YDStreamExtractor
-import YoutubeDLWrapper
-from YoutubeDLWrapper import youtube_dl
+import xbmcgui
 
-#pulsar for torrent streaming
-#import pulsar
+# hack to make youtube_dl work with xbmc redirected stderr
+sys.stderr.isatty = lambda: True
 
-
-__addon__       = xbmcaddon.Addon(id='plugin.service.youtube-dl-rcv')
+__addon__       = xbmcaddon.Addon()
 __addonname__   = __addon__.getAddonInfo('name')
 __icon__        = __addon__.getAddonInfo('icon')
 
-def build_name(info,path="",template='%(title)s-%(id)s.%(ext)s'):
-    """
-    Download the selected video in vidinfo to path.
-    Template sets the youtube-dl format which defaults to TITLE-ID.EXT.
-    """
-    # build name
-    info = YDStreamExtractor._convertInfo(info) #Get the right format
-    try:
-        YDStreamExtractor._completeInfo(info) #Make sure we have the needed bits
-    except TypeError:
-        print 'STRANGE ERROR FROM YOUTUBE DL CONTROL WHEN TRYING TO COMPLETE VIDEO INFO'
-    path_template = os.path.join(path,template)
-    ytdl = YoutubeDLWrapper._getYTDL()
-    ytdl.params['quiet'] = True
-    ytdl.params['outtmpl'] = path_template    
-    name = ytdl.prepare_filename(info)
-    name = name.encode('utf8')
-    return name
-
-def play(url):
-    '''play media in given page url'''
-    # get parameters
-    quality = __addon__.getSetting('quality')
-    qualities = {'SD': 0, '720p': 1, '1080p': 2} 
-    quality = qualities[quality]
-
-    # build media representation from url
-    info = YDStreamExtractor.getVideoInfo(url,quality=quality)
-    if info is None:
-        return False
+class extractor(threading.Thread):
+    """deamon extracting information from a media page url
+    and putting it in a queue"""
     
-    name = build_name(info)
-    stream_url = info.streamURL()
+    def __init__(self, url, q):
+        self.url = url
+        self.q = q
+        self.t = time.time()
+        threading.Thread.__init__(self)
 
-    web_player = xbmc.Player()
-    print 'LAUNCHING DISTANT VIDEO', name
-    web_player.play(stream_url)
+    def run(self):
+        ydl_opts = {
+            'ignoreerrors': True,
+            'noplaylist': True
+            }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            try:
+                info = ydl.extract_info(self.url, download=False)
+            except youtube_dl.utils.DownloadError:
+                print 'Error while streaming from '+self.url
+            else:
+                if info != None:
+                    try:
+                        stream_url = info['url']
+                    except KeyError:
+                        if info['_type'] == 'playlist':
+                            info = info['entries'][0]
+                    stream_url = info['url']
+                    name = info['title']+'-'+info['id']
+                    self.q.put((name, self.url, stream_url, self.t))
+
+class handler(BaseHTTPRequestHandler):
+    '''simple handler reacting to requests'''
     
-    #keep trace of watching activty
-    write_log(name, url)
-    
-    return True
-
-def write_log(name, url):
-    '''add watched video info in lof file'''
-    log_path = __addon__.getSetting('logfile')
-    if log_path != "":
-        with open(log_path,'a') as log:
-            time = str(datetime.datetime.now())
-            log.write(time+'\t'+name+'\t'+url+'\n')
-
-class MyHandler(BaseHTTPRequestHandler):
-    '''simple server to get watching requests'''
     def do_GET(self):
         # if receiving watch instruction
         if self.path.startswith('/watch?url='):
             url = self.path[11:]
+            
+            dialog = xbmcgui.Dialog()
+            dialog.notification('Casting', url, xbmcgui.NOTIFICATION_INFO, 5000)
             
             # send http response
             self.send_response(200)
             self.send_header('Content-type',	'text/html')
             self.end_headers()
             self.wfile.write("""<html>
-            <head>
-            <title>""" + url + """</title>
-            </head>
-            <body>
-            Trying to play """ + url + """ ...</body>
+            <head><title>""" + url + """</title></head>
+            <body>Trying to play """ + url + """ ...</body>
             </html>""")
             
-            # try and dl media at url
-            play(url)
+            # send request to media info extractor
+            ext = extractor(url, QUEUE)
+            ext.start()
         else:
             self.send_error(404,'File Not Found: %s' % self.path)
 
-# Interface listenning for web playing events
-host = ''
-port = int(__addon__.getSetting('port'))
-# Start the server
-server = HTTPServer((host, port), MyHandler)
-print "Starting http server for web media requests on port " + str(port)
-server.serve_forever()
 
-#~ url = "http://www.youtube.com/watch?v=_yVv9dx88x0"
-#~ url = "http://www.dailymotion.com/video/x29ejto_master-of-the-universe-replay_shortfilms"
-#~ url = "http://www.dailymotion.com/video/x2j23bs_watermelon-in-30-seconds-or-less_lifestyle"
-#~ url = "https://www.youtube.com/watch?v=bJmOUBlgjqA"
+class server(threading.Thread):
+    """deamon http server for new media requests"""
+    
+    def run(self):
+        # Interface listenning for web playing events
+        host = ''
+        port = int(__addon__.getSetting('port'))
+        # Start the server
+        server = HTTPServer((host, port), handler)
+        print "Starting http server for web media requests on port " + str(port)
+        server.serve_forever()
+
+def write_log(name, url):
+    '''add watched video info in log file'''
+    
+    log_path = __addon__.getSetting('logfile')
+    if log_path != "":
+        with io.open(log_path,'a', encoding='utf-8') as log:
+            time = str(datetime.datetime.now())
+            log.write(time+'\t'+name+'\t'+url+'\n')
+
+    
+try:
+    # native youtube dl
+    import youtube_dl
+except ImportError:
+    print "Missing youtube_dl in python path (sudo pip install youtube_dl)"
+else:
+    # the player to control
+    web_player = xbmc.Player()
+    # global queue for multithreading
+    QUEUE = Queue.Queue()
+    # http server to watch for requests
+    httpd = server(QUEUE)
+    httpd.start()
+    # main loop
+    last_request = 0
+    while True:
+        # watch for requests and launch player
+        if not QUEUE.empty():
+            name, url, stream_url, t = QUEUE.get()
+            # accept only last request
+            if t > last_request:
+                last_request = t
+                # send media url to kodi player
+                web_player.play(stream_url)
+                # keep trace of watching activty
+                write_log(name, url)
+        # sleep to preserve cpu
+        time.sleep(0.5)
+
