e329fd57311b934019b3318fe8409b5e027d8c4d
Glen <glen@mailoo.org>
2016-02-08 20:22:22 +0100

    now supporting playlists

--- service.py
+++ service.py
@@ -33,26 +33,47 @@ class extractor(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        ydl_opts = {
-            'ignoreerrors': True,
-            'noplaylist': True
-            }
+        is_playlist = False #are we dealing with a single media or a playlist page
+        launched = False # is a media launched ?
+        finished = False
+        i = 1
+        while not finished:
+            ydl_opts = {
+                'ignoreerrors': True,
+                'playliststart': i,
+                'playlistend': i #get only next item
+                }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                try:
+                    info = ydl.extract_info(self.url, download=False)
+                except youtube_dl.utils.DownloadError:
+                    xbmc.log('Error while streaming from '+self.url)
+                else:
+                    if info != None:
+                        try:
+                            stream_url = info['url']
+                        except KeyError:
+                            if info['_type'] == 'playlist':
+                                is_playlist = True
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            try:
-                info = ydl.extract_info(self.url, download=False)
-            except youtube_dl.utils.DownloadError:
-                print 'Error while streaming from '+self.url
-            else:
-                if info != None:
-                    try:
-                        stream_url = info['url']
-                    except KeyError:
-                        if info['_type'] == 'playlist':
-                            info = info['entries'][0]
-                    stream_url = info['url']
-                    name = info['title']+'-'+info['id']
-                    self.q.put((name, self.url, stream_url, self.t))
+                        if is_playlist:
+                            if len(info['entries']) > 0 :
+                                info = info['entries'][0]
+                            else:
+                                finished = True
+                                break
+
+                        if info != None:
+                            stream_url = info['url']
+                            name = info['title']+'-'+info['id']
+                            if launched:
+                                self.q.put(('add', name, self.url, stream_url, self.t))
+                            else:
+                                self.q.put(('play', name, self.url, stream_url, self.t))
+                                launched = True
+                                if not is_playlist:
+                                    finished = True
+            i += 1
 
 class handler(BaseHTTPRequestHandler):
     '''simple handler reacting to requests'''
@@ -90,7 +111,7 @@ class server(threading.Thread):
         port = int(__addon__.getSetting('port'))
         # Start the server
         server = HTTPServer((host, port), handler)
-        print "Starting http server for web media requests on port " + str(port)
+        xbmc.log( "Starting http server for web media requests on port " + str(port))
         server.serve_forever()
 
 def write_log(name, url):
@@ -102,15 +123,29 @@ def write_log(name, url):
             time = str(datetime.datetime.now())
             log.write(time+'\t'+name+'\t'+url+'\n')
 
+def clear_playlists( mode="both" ):
+    # clear playlists
+    if mode in ( "video", "both" ):
+        vplaylist = xbmc.PlayList( xbmc.PLAYLIST_VIDEO )
+        vplaylist.clear()
+        xbmc.log( "Video Playlist Cleared", xbmc.LOGNOTICE )
+    if mode in ( "music", "both" ):
+        mplaylist = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+        mplaylist.clear()
+        xbmc.log( "Music Playlist Cleared", xbmc.LOGNOTICE )
+  
+
     
 try:
     # native youtube dl
     import youtube_dl
 except ImportError:
-    print "Missing youtube_dl in python path (sudo pip install youtube_dl)"
+    xbmc.log( "Missing youtube_dl in python path (sudo pip install youtube_dl)")
 else:
     # the player to control
     web_player = xbmc.Player()
+    video_playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+    #~ audio_playlist = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
     # global queue for multithreading
     QUEUE = Queue.Queue()
     # http server to watch for requests
@@ -122,16 +157,34 @@ else:
     while True:
         # watch for requests and launch player
         if not QUEUE.empty():
-            name, url, stream_url, t = QUEUE.get()
-            # accept only last request
-            if t > last_request:
-                last_request = t
-                # send media url to kodi player
-                web_player.play(stream_url)
-                # keep trace of watching activty
-                write_log(name, url)
+            entry = QUEUE.get()
+            mode, name, url, stream_url, t = entry
+            #are we dealing with a single item ?
+            if mode == 'play':
+                # accept only last request
+                if t > last_request:
+                    last_request = t
+                    # clear playlists
+                    clear_playlists()
+                    # send media url to kodi player
+                    #add entry to Kodi playlist
+                    listitem = xbmcgui.ListItem()
+                    listitem.setLabel(name)
+                    video_playlist.add(stream_url, listitem)
+                    web_player.play(video_playlist)
+                    # keep trace of watching activty
+                    write_log(name, url)
+            # or a playlist item to add ? BEWARE we do not log these
+            elif mode == 'add':
+                mode, name, url, stream_url, t = entry
+                # complete only currently active playlist
+                if t == last_request:
+                    # add entry to Kodi playlist
+                    listitem = xbmcgui.ListItem()
+                    listitem.setLabel(name)
+                    video_playlist.add(stream_url, listitem)
         # reload ytdl module as it may have been updated by cron
-        period = 15*60
+        period = 15*60  #every 15 minutes
         if time.time() > flip + period:
             flip = time.time()
             youtube_dl = reload(youtube_dl)
